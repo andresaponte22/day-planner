5ba5d0aea0be4affa02ef797f21b1ba6c4dd0200
Andres Aponte <andresaponte.f@gmail.com>
2021-06-07 16:37:41 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Global vars
+
+// currentTime var to show current time with moment.js
+
+// loop for the middle row comparing it to the currentTime var, change color classes
+
+// general listener for all the save buttons
+// save the text area next to the button by using sibling or pair
+// save that text content into my local storage
+// keys=time, data=textContent of that element
+
+// function to prevent local storage clear on refresh
